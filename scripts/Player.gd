f349e5bcb3029ef5a3e91bf29613a0c66414e797
Sebastien <sebastienfct@gmail.com>
2023-01-14 23:03:50 -0500

    updates player

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -24,22 +24,15 @@ func _process(delta):
 		
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
-		$AnimatedSprite.play()
-	else:
-		$AnimatedSprite.stop()
 		
 	position += velocity * delta
 	position.x = clamp(position.x, 0, screen_size.x)
 	position.y = clamp(position.y, 0, screen_size.y)
-	
-	if velocity.x != 0:
-		$AnimatedSprite.animation = "walk"
-		$AnimatedSprite.flip_v = false
-		$AnimatedSprite.flip_h = velocity.x < 0
 		
-	if velocity.y != 0:
-		$AnimatedSprite.animation = "up"
-		$AnimatedSprite.flip_v = velocity.y > 0
+	if velocity.y > 0:
+		$AnimatedSprite.animation = "boost"
+	else:
+		$AnimatedSprite.animation = "slow"
 
 
 func _on_Player_body_entered(body):
