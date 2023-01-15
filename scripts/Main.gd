6c6c8f27e511268893f339b73e03804eeca33937
Sebastien <sebastienfct@gmail.com>
2023-01-14 20:06:37 -0500

    Finishing up

--- scripts/Main.gd
+++ scripts/Main.gd
@@ -16,6 +16,8 @@ func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
 	$HUD.show_game_over()
+	$Music.stop()
+	$DeathSound.play()
 
 func new_game():
 	get_tree().call_group("mobs", "queue_free")
@@ -24,6 +26,7 @@ func new_game():
 	$StartTimer.start()
 	$HUD.update_score(score)
 	$HUD.show_message("Get Ready")
+	$Music.play()
 
 func _on_StartTimer_timeout():
 	$MobTimer.start()
